0f4c96c16639f3d2952420ed25809ceeaaa2b6a9
Jordan Reed <jordanr23@gmail.com>
2018-01-10 15:56:06 -0700

    all tests passing

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -33,13 +33,31 @@ class BinarySearchTree {
 	}
 
 	dfs(cb, node=this.root) {
-    // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
+		// Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
+		cb(node.data)
+		if(node.left){
+			this.dfs(cb, node.left)
+		}if(node.right){
+			this.dfs(cb, node.right)
+		}
 	}
 
 	bfs(cb, node=this.root) {
-    // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
-    // Expected order: 25, 15, 197, 17, 97, 9997
+		// Write an algorithm using breadth first search that takes in a callback function and calls it for every node
+		// Expected order: 25, 15, 197, 17, 97, 9997
+		var q = [];
+		q.push(node)
+		while(q.length > 0){
+			var c = q.shift();
+			cb(c.data)
+			if(c.left){
+				q.push(c.left)
+			}
+			if(c.right){
+				q.push(c.right)
+			}
+		}
 	}
 }
 
